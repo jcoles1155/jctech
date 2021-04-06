3d53bea192f52ead4834216d6d30ea448de605a3
JC Coles <jcoles1155@gmail.com>
2021-04-06 14:20:17 -0700

    moved around files

---
+++ build/precache-manifest.5f9823abc982cabde90baee0a3efa7ca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d65e22a9f0224e0259353a1ed5264b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96e02c612fc8ceef584e",
+    "url": "/static/css/2.48c3d4a3.chunk.css"
+  },
+  {
+    "revision": "c221fba81de75d08a78d",
+    "url": "/static/css/main.b4e80cbc.chunk.css"
+  },
+  {
+    "revision": "96e02c612fc8ceef584e",
+    "url": "/static/js/2.ef523895.chunk.js"
+  },
+  {
+    "revision": "c221fba81de75d08a78d",
+    "url": "/static/js/main.64f10ef6.chunk.js"
+  },
+  {
+    "revision": "3772c879b3eb7df4d74f",
+    "url": "/static/js/runtime~main.f933d74f.js"
+  },
+  {
+    "revision": "ab93b649630af766af01301ce5dc5b00",
+    "url": "/static/media/markdown-html-supported-blog.ab93b649.md"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
